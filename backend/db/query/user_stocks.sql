e266cddbaffd761865d86ad46189d8bc33a64cbe
Jacob <osarukun@live.com>
2025-11-25 16:34:20 -0500

    feat: user_stock CRUD implemented

---
+++ backend/db/query/user_stocks.sql
@@ -0,0 +1,31 @@
+-- name: CreateUserStock :exec
+INSERT INTO
+  user_stocks (user_id, stock_id, quantity)
+VALUES
+  (?, ?, ?);
+
+-- name: GetUserStock :one
+SELECT
+  *
+FROM
+  user_stocks
+WHERE
+  user_id = ?
+  AND stock_id = ?
+LIMIT
+  1;
+
+-- name: UpdateUserStock :one
+UPDATE user_stocks
+SET
+  quantity = sqlc.arg (quantity)
+WHERE
+  user_id = sqlc.arg (user_id)
+  AND stock_id = sqlc.arg (stock_id) RETURNING *;
+
+-- name: DeleteUserStock :exec
+DELETE FROM user_stocks
+WHERE
+  user_id = ?
+  AND stock_id = ?
+  AND quantity = 0;
