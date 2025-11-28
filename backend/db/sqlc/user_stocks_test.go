27ad9e3e94f0765bff6f51b8ac3eb8fa3258aaa2
Jacob <osarukun@live.com>
2025-11-28 18:22:29 -0500

    wip: userstock update test

--- backend/db/sqlc/user_stocks_test.go
+++ backend/db/sqlc/user_stocks_test.go
@@ -1,11 +1,14 @@
 package db
 
 import (
+	//stl
 	"context"
 	"testing"
 
-	"github.com/JacobButcher-Github/folk-investing/backend/util"
+	//
 	"github.com/stretchr/testify/require"
+
+	"github.com/JacobButcher-Github/folk-investing/backend/util"
 )
 
 func createRandomUserStock(t *testing.T) UserStock {
@@ -24,4 +27,35 @@ func createRandomUserStock(t *testing.T) UserStock {
 	require.Equal(t, userStock.Quantity, randQuantity)
 
 	return userStock
+}
+
+func TestCreateUserStock(t *testing.T) {
+	createRandomUserStock(t)
+}
+
+func TestGetUserStock(t *testing.T) {
+	userStock1 := createRandomUserStock(t)
+	userStock2, err := testQueries.GetUserStock(context.Background(), GetUserStockParams{
+		UserID:  userStock1.UserID,
+		StockID: userStock1.StockID,
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, userStock2)
+
+	require.Equal(t, userStock1.UserID, userStock2.UserID)
+	require.Equal(t, userStock1.StockID, userStock2.StockID)
+	require.Equal(t, userStock1.Quantity, userStock2.Quantity)
+}
+
+func TestUpdateUserStock(t *testing.T) {
+	userStock1 := createRandomUserStock(t)
+	var newQuantity int64
+	for {
+		newQuantity = util.randomInt(1, 99)
+		if newQuantity != userStock1.Quantity {
+			break
+		}
+	}
+
+	//TODO: update the userstock use the return value as userstock2, compare.
 }
