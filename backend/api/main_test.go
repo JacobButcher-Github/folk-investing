3a2c9eac53003cbd5fdc7d1a56c37328cf6ac4bc
Jacob <osarukun@live.com>
2025-12-02 20:39:15 -0500

    feat: api test suite initialized

---
+++ backend/api/main_test.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	db "home/osarukun/repos/tower-investing/backend/db/sqlc"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+)
+
+func NewTestServer(t *testing.T, store db.Store) *Server {
+
+	server, err := NewServer(store)
+	require.NoError(t, err)
+
+	return server
+}
+
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+
+	os.Exit(m.Run())
+}
