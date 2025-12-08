73b5adcf2b3cf30b23b6795560c48f47e644d08c
Jacob <osarukun@live.com>
2025-12-07 19:54:47 -0500

    feat: initial setup of all needed packages

---
+++ frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<!-- App.svelte -->
+<script lang="ts">
+  // root component logic
+</script>
+
+<main>
+  <h1>Welcome to Folk Investing</h1>
+</main>
+
+
+<style>
+  h1 {
+      color: goldenrod;
+      font-size: 2em;
+    }
+</style>
